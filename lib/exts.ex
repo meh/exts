3e4a7b582fc0d18d7101f68dfe80fff72e37571d
meh <meh@schizofreni.co>
2013-04-24 18:38:08 +0200

    doc: add documentation

--- lib/exts.ex
+++ lib/exts.ex
@@ -8,6 +8,11 @@
 
 defmodule Exts do
   defexception FileError, reason: nil do
+    @moduledoc """
+    Exception thrown if an error occurs on loading or dumping a table.
+    """
+
+    @spec message(t) :: String.t
     def message(self) do
       self.reason
     end
@@ -15,12 +20,19 @@ defmodule Exts do
 
   @type table :: integer | atom
 
+  @doc """
+  Load a table from the given file, see `ets:file2tab`.
+  """
   @spec load(String.t) :: { :ok, table } | { :error, any }
   @spec load(String.t, Keyword.t) :: { :ok, table } | { :error, any }
   def load(path, options // []) do
     :ets.file2tab(path, options)
   end
 
+  @doc """
+  Load a table from the given file, raising if there's any problem doing so,
+  see `ets:file2tab`.
+  """
   @spec load!(String.t) :: table | no_return
   @spec load!(String.t, Keyword.t) :: table | no_return
   def load!(path, options // []) do
@@ -33,12 +45,19 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Dump the given table to the given file, see `ets:tab2file`.
+  """
   @spec dump(table, String.t) :: :ok | { :error, any }
   @spec dump(table, String.t, Keyword.t) :: :ok | { :error, any }
   def dump(table, path, options // []) do
     :ets.tab2file(table, path, options)
   end
 
+  @doc """
+  Dump the given table to the given file, raising if there's any problem doing
+  so, see `ets:tab2file`.
+  """
   @spec dump!(table, String.t) :: :ok | no_return
   @spec dump!(table, String.t, Keyword.t) :: :ok | no_return
   def dump!(table, path, options // []) do
@@ -51,6 +70,10 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Get information about the given table or table saved on file, see `ets:info`
+  and `ets:tabfile_info`.
+  """
   @spec info(String.t | table) :: { :ok, any } | { :error, any } | Keyword.t | nil
   def info(path) when is_binary path do
     :ets.tabfile_info(path)
@@ -63,6 +86,10 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Get specific information about the given table and given topic, see
+  `ets:info`.
+  """
   @spec info(table, atom) :: any | nil
   def info(table, key) do
     case :ets.info(table, key) do
@@ -71,31 +98,62 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Rename the given table to the given name, see `ets:rename`.
+  """
   @spec rename(table, atom) :: atom
   def rename(table, name) do
     :ets.rename(table, name)
   end
 
+  @doc """
+  Get all the present tables, see `ets:all`.
+  """
   @spec all :: [table]
   def all do
     :ets.all
   end
 
+  @doc """
+  Convert the given table to a list of records, see `ets:tab2list`.
+  """
   @spec to_list(table) :: [record]
   def to_list(table) do
     :ets.tab2list(table)
   end
 
+  @doc """
+  Create a new unnamed table with the default options, see `ets:new`.
+  """
   @spec new :: table
   def new do
     :ets.new(nil, [])
   end
 
+  @doc """
+  Create a new unnamed table with the given options, see `ets:new`.
+  """
   @spec new(Keyword.t) :: table
   def new(options) do
     new(nil, options)
   end
 
+  @doc """
+  Create a new named table with the given options, see `ets:new`.
+
+  ## Options
+
+  * `:index` sets the position of the key in the tuple, default is 0.
+  * `:concurrency` can be either `:both`, `:write` or `:read`, it sets
+    `:write_concurrency` and `:read_concurrency` appropriately.
+  * `:type` can either be `:set`, `:ordered_set`, `:bag` or `:duplicate_bag`,
+    default is `:set`
+  * `:access` can either be `:public`, `:protected`, `:private`, default is
+    `:protected`.
+  * `:heir` sets a heir for the table, see the documentation of `ets:new`,
+     default is none.
+  * `:compressed` can be either true or false, default is false
+  """
   @spec new(atom, Keyword.t) :: table
   def new(name, options) do
     args = []
@@ -141,27 +199,42 @@ defmodule Exts do
     :ets.new(name, args)
   end
 
+  @doc """
+  Give the given table to the given process, see `ets:give_away`.
+  """
   @spec give_to(table, pid)      :: true
   @spec give_to(table, pid, any) :: true
   def give_to(table, pid, data // nil) do
     :ets.give_away(table, pid, data)
   end
 
+  @doc """
+  Clear the given table, see `ets:delete_all_objects`.
+  """
   @spec clear(table) :: true
   def clear(table) do
     :ets.delete_all_objects(table)
   end
 
+  @doc """
+  Destroy the given table, see `ets:delete`.
+  """
   @spec destroy(table) :: true
   def destroy(table) do
     :ets.delete(table)
   end
 
+  @doc """
+  Read the given record from the given table, see `ets:lookup`.
+  """
   @spec read(table, any) :: [record]
   def read(table, key) do
     :ets.lookup(table, key)
   end
 
+  @doc """
+  Read the records in the given slot, see `ets:slot`.
+  """
   @spec at(table, integer) :: [record]
   def at(table, slot) do
     case :ets.slot(table, slot) do
@@ -170,6 +243,9 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Get the first key in the given table, see `ets:first`.
+  """
   @spec first(table) :: any | nil
   def first(table) do
     case :ets.first(table) do
@@ -178,6 +254,9 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Get the next key in the given table, see `ets:next`.
+  """
   @spec next(table, any) :: any | nil
   def next(table, key) do
     case :ets.next(table, key) do
@@ -186,6 +265,9 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Get the previous key in the given table, see `ets:prev`.
+  """
   @spec prev(table, any) :: any | nil
   def prev(table, key) do
     case :ets.prev(table, key) do
@@ -194,6 +276,9 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Get the last key in the given table, see `ets:prev`.
+  """
   @spec last(table) :: any | nil
   def last(table) do
     case :ets.last(table) do
@@ -203,10 +288,20 @@ defmodule Exts do
   end
 
   defmodule Selection do
+    @moduledoc """
+    Selection wraps an `ets:select` result, which may or may not contain a
+    continuation, in case of continuations you can access the next set of
+    values by calling `.next`.
+    """
+
     @opaque t :: record
 
     defrecordp :selection, values: [], continuation: nil, reverse: false
 
+    @doc """
+    Get a Selection from the various select results.
+    """
+    @spec new(:'$end_of_table' | list | { list, any }) :: t | nil
     def new(value, reverse // false) do
       case value do
         :'$end_of_table' -> nil
@@ -218,14 +313,27 @@ defmodule Exts do
       end
     end
 
+    @doc """
+    Check if the Selection is traversing in reverse.
+    """
+    @spec reverse?(t) :: boolean
     def reverse?(selection(reverse: reverse)) do
       reverse
     end
 
+    @doc """
+    Get the values in the current Selection.
+    """
+    @spec values(t) :: [any]
     def values(selection(values: v)) do
       v
     end
 
+    @doc """
+    Get the next set of values wrapped in another Selection, returns nil if
+    there are no more.
+    """
+    @spec next(t) :: t | nil
     def next(selection(continuation: nil)) do
       nil
     end
@@ -239,27 +347,54 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Select records in the given table using a match_spec, see `ets:select`.
+  """
+  @spec select(table, any) :: Selection.t | nil
   def select(table, match_spec) do
     Selection.new(:ets.select(table, match_spec))
   end
 
+  @doc """
+  Select records in the given table using a match_spec passing a limit, see
+  `ets:select`.
+  """
+  @spec select(table, non_neg_integer, any) :: Selection.t | nil
   def select(table, limit, match_spec) when is_integer limit do
     Selection.new(:ets.select(table, match_spec, limit))
   end
 
+  @doc """
+  Select records in the given table using a match_spec, traversing in reverse,
+  see `ets:select_reverse`.
+  """
+  @spec reverse_select(table, any) :: Selection.t | nil
   def reverse_select(table, match_spec) do
     Selection.new(:ets.select_reverse(table, match_spec), true)
   end
 
+  @doc """
+  Select records in the given table using a match_spec passing a limit,
+  traversing in reverse, see `ets:select_reverse`.
+  """
   def reverse_select(table, limit, match_spec) do
     Selection.new(:ets.select_reverse(table, match_spec, limit), false)
   end
 
   defmodule Match do
+    @moduledoc """
+    Match wraps an `ets:match` or `ets:match_object` result, which may or may
+    not contain a continuation, in case of continuations you can access the
+    next set of values by calling `.next`.
+    """
+
     @opaque t :: record
 
     defrecordp :match, values: [], continuation: nil, whole: false
 
+    @doc """
+    Get a Match from the various match results.
+    """
     def new(value, whole // false) do
       case value do
         :'$end_of_table' -> nil
@@ -271,14 +406,27 @@ defmodule Exts do
       end
     end
 
+    @doc """
+    Check if the Match is matching whole objects.
+    """
+    @spec whole?(t) :: boolean
     def whole?(match(whole: whole)) do
       whole
     end
 
+    @doc """
+    Get the values in the current Match.
+    """
+    @spec values(t) :: [any]
     def values(match(values: v)) do
       v
     end
 
+    @doc """
+    Get the next set of values wrapped in another Match, returns nil if there
+    are no more.
+    """
+    @spec next(t) :: Match.t | nil
     def next(match(continuation: nil)) do
       nil
     end
@@ -292,11 +440,24 @@ defmodule Exts do
     end
   end
 
+  @doc """
+  Match records from the given table with the given pattern, see `ets:match`.
+  """
   @spec match(table, any) :: Match.t | nil
   def match(table, pattern) do
     Match.new(:ets.match(table, pattern))
   end
 
+  @doc """
+  Match records from the given table with the given pattern and options or
+  limit, see `ets:match`.
+
+  ## Options
+
+  * `:whole` when true it returns the whole record.
+  * `:delete` when true it deletes the matching records instead of returning
+    them.
+  """
   @spec match(table, any | integer, Keyword.t | any) :: Match.t | nil
   def match(table, pattern, delete: true) do
     :ets.match_delete(table, pattern)
@@ -310,31 +471,51 @@ defmodule Exts do
     Match.new(:ets.match(table, pattern, limit))
   end
 
+  @doc """
+  Match record from the given table with the given pattern, options and limit,
+  see `ets:match_object`.
+  """
   @spec match(table, integer, any, Keyword.t) :: Match.t | nil
   def match(table, limit, pattern, whole: true) do
     Match.new(:ets.match_object(table, pattern, limit))
   end
 
+  @doc """
+  Get the number of records in the given table.
+  """
   @spec count(table) :: non_neg_integer
   def count(table) do
     info(table, :size)
   end
 
+  @doc """
+  Count the number of records matching the match_spec, see `ets:select_count`.
+  """
   @spec count(table, any) :: non_neg_integer
   def count(table, match_spec) do
     :ets.select_count(table, match_spec)
   end
 
+  @doc """
+  Fold the given table from the left, see `ets:foldl`.
+  """
   @spec foldl(table, any, (record, any -> any)) :: any
   def foldl(table, acc, fun) do
     :ets.foldl(fun, acc, table)
   end
 
+  @doc """
+  Fold the given table from the right, see `ets:foldr`.
+  """
   @spec foldr(table, any, (record, any -> any)) :: any
   def foldr(table, acc, fun) do
     :ets.foldr(fun, acc, table)
   end
 
+  @doc """
+  Delete the record matching the given pattern or key in the given table, see
+  `ets:select_delete` and `ets:delete`.
+  """
   @spec delete(table, any) :: true | integer
   def delete(table, [{ _, _, _ } | _] = match_spec) do
     :ets.select_delete(table, match_spec)
@@ -344,6 +525,9 @@ defmodule Exts do
     :ets.delete(table, key)
   end
 
+  @doc """
+  Delete the given record from the given table, see `ets:delete_object`.
+  """
   @spec delete!(table, record) :: true
   def delete!(table, object) do
     :ets.delete_object(table, object)
@@ -351,6 +535,10 @@ defmodule Exts do
 
   # TODO: udpate_counter and update_element
 
+  @doc """
+  Write the given record to the given table optionally disabling overwriting,
+  see `ets:insert` and `ets:insert_new`.
+  """
   @spec write(table, record, Keyword.t) :: boolean
   def write(table, object, options // []) do
     if options[:overwrite] == false do
--- lib/exts/table.ex
+++ lib/exts/table.ex
@@ -11,11 +11,26 @@ defmodule Exts.Table do
 
   defrecordp :table, id: nil, type: nil, resource: nil
 
+  @doc """
+  Create a new table with default options.
+  """
   @spec new :: t
   def new do
     new([])
   end
 
+  @doc """
+  Wrap a table or create one with the passed options.
+
+  If automatic mode isn't disabled, smart garbage collection will be used,
+  access will be forced to public and heir will be forced to Exts.Manager.
+
+  Smart garbage collection implies tables won't die with processes and they
+  will be destroyed when they aren't referenced by anything anymore.
+
+  The Exts.Manager process will be set as heir and you'll be able to retake
+  ownership of a table if wanted.
+  """
   @spec new(integer | atom | Keyword.t) :: t
   def new(options) when is_list options do
     if options[:automatic] != false do
@@ -37,21 +52,36 @@ defmodule Exts.Table do
     table(id: id, type: :ets.info(id, :type))
   end
 
+  @doc """
+  Get the id of the table, usable with the raw :ets functions or Exts wrapped
+  ones.
+  """
+  @spec id(t) :: integer | atom
   def id(table(id: id)) do
     id
   end
 
+  @doc """
+  Rename the table to the given atom, see `ets:rename`.
+  """
   @spec rename(atom, t) :: atom
   def rename(name, table(id: id)) do
     Exts.rename(id, name)
   end
 
+  @doc """
+  Give the table to another process, optionally passing data to give to the
+  process, see `ets:give_away`.
+  """
   @spec give_to(pid, t) :: true
   @spec give_to(pid, any, t) :: true
   def give_to(pid, data // nil, table(id: id)) do
     Exts.give_to(id, pid, data)
   end
 
+  @doc """
+  Load a table from a file, see `ets:file2tab`.
+  """
   @spec load(String.t)            :: { :ok, t } | { :error, any }
   @spec load(String.t, Keyword.t) :: { :ok, t } | { :error, any }
   def load(path, options // []) do
@@ -64,6 +94,10 @@ defmodule Exts.Table do
     end
   end
 
+  @doc """
+  Load a table from a file, raising if there's a problem with the file, see
+  `ets:file2tab`.
+  """
   @spec load!(String.t)            :: t | no_return
   @spec load!(String.t, Keyword.t) :: t | no_return
   def load!(path, options // []) do
@@ -76,12 +110,18 @@ defmodule Exts.Table do
     end
   end
 
+  @doc """
+  Dump the table to a file.
+  """
   @spec dump(String.t, t) :: :ok | { :error, any }
   @spec dump(String.t, Keyword.t, t) :: :ok | { :error, any }
   def dump(path, options // [], table(id: id)) do
     :ets.tab2file(id, path, options)
   end
 
+  @doc """
+  Dump the table to a file, raising if there's a problem while doing so.
+  """
   @spec dump!(String.t, t) :: :ok | no_return
   @spec dump!(String.t, Keyword.t, t) :: :ok | no_return
   def dump!(path, options // [], table(id: id)) do
@@ -94,6 +134,10 @@ defmodule Exts.Table do
     end
   end
 
+  @doc """
+  Protect the table while running the passed function, useful to make iterating
+  safe, see `ets:safe_fixtable`.
+  """
   @spec protect(function, t) :: any
   def protect(fun, table(id: id) = self) do
     :ets.safe_fixtable(id, true)
@@ -108,46 +152,73 @@ defmodule Exts.Table do
     end
   end
 
+  @doc """
+  Check if the table is a bag.
+  """
   @spec bag?(t) :: boolean
   def bag?(table(type: type)) do
     type == :bag
   end
 
+  @doc """
+  Check if the table is a duplicate bag.
+  """
   @spec duplicate_bag?(t) :: boolean
   def duplicate_bag?(table(type: type)) do
     type == :duplicate_bag
   end
 
+  @doc """
+  Check if the table is a set.
+  """
   @spec set?(t) :: boolean
   def set?(table(type: type)) do
     type == :set
   end
 
+  @doc """
+  Check if the table is an ordered set.
+  """
   @spec ordered_set?(t) :: boolean
   def ordered_set?(table(type: type)) do
     type == :ordered_set
   end
 
+  @doc """
+  Get info about the table, see `ets:info`.
+  """
   @spec info(t, atom) :: any | nil
   def info(key, table(id: id)) do
     Exts.info(id, key)
   end
 
+  @doc """
+  Convert the table to a list, see `ets:tab2list`.
+  """
   @spec to_list(t) :: [record]
   def to_list(table(id: id)) do
     Exts.to_list(id)
   end
 
+  @doc """
+  Clear the contents of the table, see `ets:delete_all_objects`.
+  """
   @spec clear(t) :: true
   def clear(table(id: id)) do
     Exts.clear(id)
   end
 
+  @doc """
+  Destroy the table, see `ets:delete`.
+  """
   @spec destroy(t) :: true
   def destroy(table(id: id)) do
     Exts.destroy(id)
   end
 
+  @doc """
+  Check if the table contains the given key.
+  """
   @spec contains?(any, t) :: boolean
   def contains?(key, table(id: id)) do
     case Exts.read(id, key) do
@@ -156,6 +227,10 @@ defmodule Exts.Table do
     end
   end
 
+  @doc """
+  Read a records from the table, if it's a set or ordered set it returns a
+  single record, otherwise it returns a list of records, see `ets:lookup`.
+  """
   @spec read(any, t) :: [record] | record
   def read(key, table(id: id, type: type)) when type in [:bag, :duplicate_bag] do
     case Exts.read(id, key) do
@@ -168,102 +243,176 @@ defmodule Exts.Table do
     Enum.first Exts.read(id, key)
   end
 
+  @doc """
+  Read the records in the given slot, see `ets:slot`.
+  """
   @spec at(integer, t) :: [record]
   def at(slot, table(id: id)) do
     Exts.at(id, slot)
   end
 
+  @doc """
+  Get the first key in table, see `ets:first`.
+  """
   @spec first(t) :: any
   def first(table(id: id)) do
     Exts.first(id)
   end
 
+  @doc """
+  Get the next key in the table, see `ets:next`.
+  """
   @spec next(any, t) :: any
   def next(key, table(id: id)) do
     Exts.next(id, key)
   end
 
+  @doc """
+  Get the previous key in the table, see `ets:prev`.
+  """
   @spec prev(any, t) :: any
   def prev(key, table(id: id)) do
     Exts.prev(id, key)
   end
 
+  @doc """
+  Get the last key in the table, see `ets:last`.
+  """
   @spec last(t) :: any
   def last(table(id: id)) do
     Exts.last(id)
   end
 
+  @doc """
+  Select records in the table using a match_spec, see `ets:select`.
+  """
   @spec select(any, t) :: [any]
   def select(match_spec, table(id: id)) do
     Exts.select(id, match_spec)
   end
 
+  @doc """
+  Select records in the table using a match_spec and passing a limit,
+  `ets:select`.
+  """
   @spec select(integer, any, t) :: [any]
   def select(limit, match_spec, table(id: id)) do
     Exts.select(limit, id, match_spec)
   end
 
+  @doc """
+  Select records in the table using a match_spec, traversing in reverse, see
+  `ets:select_reverse`.
+  """
   @spec reverse_select(any, t) :: [any]
   def reverse_select(match_spec, table(id: id)) do
     Exts.reverse_select(id, match_spec)
   end
 
+  @doc """
+  Select records in the table using a match_spec and passing a limit,
+  traversing in reverse, `ets:select_reverse`.
+  """
   @spec reverse_select(integer, any, t) :: [any]
   def reverse_select(limit, match_spec, table(id: id)) do
     Exts.reverse_select(limit, id, match_spec)
   end
 
+  @doc """
+  Match records from the table with the given pattern, see `ets:match`.
+  """
   @spec match(any, t) :: Match.t | nil
   def match(pattern, table(id: id)) do
     Exts.match(id, pattern)
   end
 
+  @doc """
+  Match records from the table with the given pattern and options or limit, see
+  `ets:match`.
+  """
   @spec match(any | integer, Keyword.t | any, t) :: Match.t | nil
   def match(limit_or_pattern, options_or_pattern, table(id: id)) do
     Exts.match(id, limit_or_pattern, options_or_pattern)
   end
 
+  @doc """
+  Match records from the table with the given pattern, options and limit, see
+  `ets:match`.
+  """
   @spec match(integer, any, Keyword.t, t) :: Match.t | nil
   def match(limit, pattern, options, table(id: id)) do
     Exts.match(id, limit, pattern, options)
   end
 
+  @doc """
+  Get the number of records in the table.
+  """
+  @spec count(t) :: non_neg_integer
   def count(table(id: id)) do
     Exts.count(id)
   end
 
+  @doc """
+  Count the number of records matching the match_spec, see `ets:select_count`.
+  """
+  @spec count(any, t) :: non_neg_integer
   def count(spec, table(id: id)) do
     Exts.count(id, spec)
   end
 
+  @doc """
+  Fold the table from the left, see `ets:foldl`.
+  """
   @spec foldl(any, (record, any -> any), t) :: any
   def foldl(acc, fun, table(id: id)) do
     Exts.foldl(id, acc, fun)
   end
 
+  @doc """
+  Fold the table from the right, see `ets:foldr`.
+  """
   @spec foldr(any, (record, any -> any), t) :: any
   def foldr(acc, fun, table(id: id)) do
     Exts.foldr(id, acc, fun)
   end
 
+  @doc """
+  Return an iterator for the table.
+  """
+  @spec iterator(t) :: Exts.Table.Iterator.t
   def iterator(self) do
     Exts.Table.Iterator.new(self, reverse: false)
   end
 
+  @doc """
+  Return a reverse iterator for the table.
+  """
+  @spec reverse_iterator(t) :: Exts.Table.Iterator.t
   def reverse_iterator(self) do
     Exts.Table.Iterator.new(self, reverse: true)
   end
 
+  @doc """
+  Delete the record matching the given match_spec or key, see
+  `ets:select_delete` and `ets:delete`.
+  """
   @spec delete(any, t) :: true
   def delete(key_or_pattern, table(id: id)) do
     Exts.delete(id, key_or_pattern)
   end
 
+  @doc """
+  Delete the given record from the table, see `ets:delete_object`.
+  """
   @spec delete!(record, t) :: true
   def delete!(object, table(id: id)) do
     Exts.delete!(id, object)
   end
 
+  @doc """
+  Write the given record to the table optionally disabling overwriting, see
+  `ets:insert` and `ets:insert_new`.
+  """
   @spec write(record, t)            :: boolean
   @spec write(record, Keyword.t, t) :: boolean
   def write(object, options // [], table(id: id)) do
@@ -277,7 +426,6 @@ defimpl Access, for: Exts.Table do
   end
 end
 
-# TODO: more performant iterator, seriously
 defimpl Enum.Iterator, for: Exts.Table do
   def iterator(self) do
     Enum.Iterator.iterator(self.iterator)
