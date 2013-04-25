c4478577b880871c1680cb2601f6a9d98176a045
meh <meh@schizofreni.co>
2013-04-25 10:01:37 +0200

    manager: add documentation

--- lib/exts/manager.ex
+++ lib/exts/manager.ex
@@ -7,9 +7,17 @@
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 defmodule Exts.Manager do
+  @moduledoc """
+  This module manages smart garbage collection for ets tables.
+  """
+
   use Application.Behaviour
   use GenServer.Behaviour
 
+  @doc """
+  Start the manager, if it's already started it will just return the original
+  process.
+  """
   def start(_, _) do
     if pid = Process.whereis(__MODULE__) do
       { :ok, pid }
@@ -24,20 +32,33 @@ defmodule Exts.Manager do
     end
   end
 
+  @doc """
+  Stop the manager, killing the process, keep in mind this will terminate the
+  managed tables too.
+  """
   def stop(_) do
     Process.exit(Process.whereis(__MODULE__), "application stopped")
   end
 
+  @doc """
+  Handle a call to reown the table.
+  """
   def handle_call({ :own, id, pid }, state) do
     { :reply, :ets.give_away(id, pid, nil), List.delete(state, id) }
   end
 
+  @doc """
+  Handle the finalization.
+  """
   def handle_info({ :destroy, table }, state) do
     :ets.delete(table)
 
     { :noreply, List.delete(state, table) }
   end
 
+  @doc """
+  Handle a table transfer.
+  """
   def handle_info({ :'ETS-TRANSFER', table, _from, _data }, state) do
     { :noreply, [table | state] }
   end
