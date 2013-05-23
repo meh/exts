e8f3ac0c5bc75bc911a9a7f91b66c5c848ab56db
meh <meh@schizofreni.co>
2013-05-23 23:09:11 +0200

    package: update information

--- package.exs
+++ package.exs
@@ -1,9 +1,9 @@
 Expm.Package.new(
   name: "exts",
-  description: "Wrapper for ets",
+  description: "Elixir Terms Storage, wrapper for ets",
   version: "0.0.1",
   licenses: [[name: "WTFPL"]],
-  keywords: ["mnesia", "database"],
+  keywords: ["database", "wrapper", "ets"],
   maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
   repositories: [[github: "meh/exts", tag: "v0.0.1"]]
 )
