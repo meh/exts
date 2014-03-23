06e860e11ed1680c4d848992ff3c901c718f07d1
Christoph Grabo <chris@dinarrr.com>
2014-03-23 15:02:03 +0100

    Remove comments

--- mix.exs
+++ mix.exs
@@ -8,7 +8,6 @@ defmodule Exts.Mixfile do
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     if System.get_env("ELIXIR_NO_NIF") do
       []
@@ -18,8 +17,6 @@ defmodule Exts.Mixfile do
     end
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     if System.get_env("ELIXIR_NO_NIF") do
       [ { :datastructures, github: "meh/elixir-datastructures" } ]
