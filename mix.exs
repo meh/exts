ba74c8d624578ee2627d2505f6e4d6074083900d
meh <meh@schizofreni.co>
2014-05-11 21:07:15 +0200

    mix: update requirements and dependencies

--- mix.exs
+++ mix.exs
@@ -3,15 +3,15 @@ defmodule Exts.Mixfile do
 
   def project do
     [ app: :exts,
-      version: "0.1.1-dev",
-      elixir: "~> 0.13.2-dev",
+      version: "0.1.1",
+      elixir: "~> 0.13.2",
       package: package,
       description: "ets wrapper for Elixir" ]
   end
 
   defp package do
     [ contributors: ["meh"],
-      licenses: ["WTFPL"],
+      license: "WTFPL",
       links: [ { "GitHub", "https://github.com/meh/exts" } ] ]
   end
 end
