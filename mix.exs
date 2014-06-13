7986f202424e50088012a5de54c193dcde6df234
meh <meh@schizofreni.co>
2014-06-13 16:47:42 +0200

    mix: bump version and Elixir requirement

--- mix.exs
+++ mix.exs
@@ -3,15 +3,15 @@ defmodule Exts.Mixfile do
 
   def project do
     [ app: :exts,
-      version: "0.1.1",
-      elixir: "~> 0.13.2",
+      version: "0.1.2",
+      elixir: "~> 0.14.0",
       package: package,
       description: "ets wrapper for Elixir" ]
   end
 
   defp package do
     [ contributors: ["meh"],
-      license: "WTFPL",
+      licenses: ["WTFPL"],
       links: [ { "GitHub", "https://github.com/meh/exts" } ] ]
   end
 end
